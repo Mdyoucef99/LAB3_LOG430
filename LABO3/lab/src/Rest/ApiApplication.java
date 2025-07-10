ed5460c8653f32cf31b65cfa5968ab90b96cd753
Youcef <mekki.d.youcef@outlook.fr>
2025-07-10 16:51:12 -0400

    added packages

--- LABO3/lab/src/Rest/ApiApplication.java
+++ LABO3/lab/src/Rest/ApiApplication.java
@@ -3,7 +3,7 @@ package Rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"Rest", "dao"})
+@SpringBootApplication(scanBasePackages = {"Rest", "dao", "Service", "Model", "Controller"})
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
